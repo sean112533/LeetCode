f1d75056d1861c4651a66737e62fe7cb7bd6c33a
sean112533 <zdte141@live.rhul.ac.uk>
2020-07-14 13:17:32 +0100

    commit the addBinary

--- EasyQuestions/src/code/AddBinary.java
+++ EasyQuestions/src/code/AddBinary.java
@@ -3,11 +3,58 @@ package code;
 public class AddBinary {
 
   public String addBinary(String a, String b) {
-    String result;
+    int i = a.length() - 1;
+    int j = b.length() - 1;
+    char carry = '0';
+    String result = "";
+
+    while (i >= 0 && j >= 0) {
+      if (a.charAt(i) == '1' && b.charAt(j) == '1') {
+        result += carry == '1' ? '1' : '0';
+        carry = '1';
+      } else if (a.charAt(i) == '1' || b.charAt(j) == '1') {
+        if (carry == '1') {
+          result += '0';
+          carry = '1';
+        } else {
+          result += '1';
+        }
+      } else {
+        result += carry;
+        carry = '0';
+      }
+      i--;
+      j--;
+    }
+    while (i >= 0) {
+      if (carry == '1' && a.charAt(i) == '1') {
+        result += '0';
+        carry = '1';
+      } else {
+        result += carry == '1' ? carry : a.charAt(i);
+        carry = '0';
+      }
+      i--;
+    }
+    while (j >= 0) {
+      if (carry == '1' && b.charAt(j) == '1') {
+        result += carry == '1' ? carry : b.charAt(j);
+        carry = '0';
+      }
+      j--;
+    }
+    if (carry == '1')
+      result += '1';
+    String str = "";
+    for (int r = result.length() - 1; r >= 0; r--) {
+      str += result.charAt(r);
+    }
+    return str;
 
   }
 
   public static void main(String[] args) {
-
+    AddBinary test = new AddBinary();
+    test.addBinary("11", "1");
   }
 }
--- EasyQuestions/src/test/TestAddBinary.java
+++ EasyQuestions/src/test/TestAddBinary.java
@@ -1,5 +1,6 @@
 package test;
 
+import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
 import code.AddBinary;
@@ -13,8 +14,22 @@ public class TestAddBinary {
   @Test
   public void testAddBinary() {
     AddBinary test = new AddBinary();
-    String a;
-    String b;
-    assertTrue("if the addition of two binary number is correct", test.addBinary(a, b) == "100");
+    String a = "11";
+    String b = "1";
+    String result = "100";
+    assertTrue("if the addition of two binary number is correct",
+        test.addBinary(a, b).equals(result));
   }
+
+  @Test
+  public void testAgain() {
+    AddBinary test = new AddBinary();
+    String c = "1010";
+    String d = "1011";
+    String result = "10101";
+    assertTrue("if  the two binary number add up correctly return true",
+        test.addBinary(c, d).equals(result));
+
+  }
+
 }
