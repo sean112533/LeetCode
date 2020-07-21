9744ff8a88eabec0eabcfd3894372834423bd6cd
sean112533 <zdte141@live.rhul.ac.uk>
2020-07-21 13:31:35 +0100

    commit the MergeSortedArray

---
+++ EasyQuestions/src/test/TestMergeSortedArray.java
@@ -0,0 +1,18 @@
+package test;
+
+import org.junit.Before;
+import org.junit.Test;
+import code.MergeSortedArray;
+
+public class TestMergeSortedArray {
+  @Before
+  public void testClass() {
+    MergeSortedArray test = new MergeSortedArray();
+
+  }
+
+  @Test
+  public void testMerge() {
+
+  }
+}
