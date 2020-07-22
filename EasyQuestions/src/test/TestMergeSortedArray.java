438e12ebb4899847a4a151d4e968b2ed7079f2bb
sean112533 <zdte141@live.rhul.ac.uk>
2020-07-22 11:13:06 +0100

    try again for the mergeSortedArray

--- EasyQuestions/src/test/TestMergeSortedArray.java
+++ EasyQuestions/src/test/TestMergeSortedArray.java
@@ -8,11 +8,17 @@ public class TestMergeSortedArray {
   @Before
   public void testClass() {
     MergeSortedArray test = new MergeSortedArray();
-
   }
 
   @Test
   public void testMerge() {
-
+    int[] nums1 = {1, 2, 3, 0, 0, 0};
+    int[] nums2 = {2, 5, 6};
+    int m = 3;
+    int n = 3;
+    int[] output = {1, 2, 2, 3, 5, 6};
+    MergeSortedArray test = new MergeSortedArray();
+    assertEuqals("if two given integer array merge as one sorted array",
+        test.merge(nums1, m, nums2, n).compareTo(output));
   }
 }
