4c542434aa64e6a75397c7b6377657b26142ffed
sean112533 <zdte141@live.rhul.ac.uk>
2020-07-22 12:34:43 +0100

    commit the leetCode question 100 Same Tree

---
+++ EasyQuestions/src/code/SameTree.java
@@ -0,0 +1,16 @@
+package code;
+
+/**
+ * LeetCode Easy question 100.Same Tree.
+ * 
+ * @author sanghunlee
+ *
+ */
+public class SameTree {
+
+  public boolean isSameTree(TreeNode p, TreeNode q) {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+}
---
+++ EasyQuestions/src/test/TestSameTree.java
@@ -0,0 +1,11 @@
+package test;
+
+import org.junit.Before;
+import code.SameTree;
+
+public class TestSameTree {
+  @Before
+  public void testClass() {
+    SameTree test = new SameTree();
+  }
+}
