c0e1c20e568a3f3936a0aaff270df0570e45d493
sean112533 <zdte141@live.rhul.ac.uk>
2020-07-27 16:16:12 +0100

    commit the method isSymmetric()

--- EasyQuestions/src/code/SymmetricTree.java
+++ EasyQuestions/src/code/SymmetricTree.java
@@ -9,12 +9,29 @@ package code;
 public class SymmetricTree {
 
   public boolean isSymmetric(TreeNode root) {
-    // TODO Auto-generated method stub
-    return false;
+    if (root == null) {
+      return true;
+    }
+    return isMirror(root.left, root.right);
+  }
+
+  public boolean isMirror(TreeNode t1, TreeNode t2) {
+    if (t1 == null && t2 == null) {
+      return true;
+    }
+    if (t1 == null || t2 == null) {
+      return false;
+    }
+    if (t1.val != t2.val) {
+      return false;
+    }
+    return isMirror(t1.left, t2.right) && isMirror(t1.right, t2.right);
   }
 
   public static void main(String[] args) {
+    SymmetricTree test = new SymmetricTree();
+
+    test.isSymmetric(root);
 
   }
-
 }
