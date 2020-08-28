0aedd684104a12d316d22750aa13b1734a0b23f8
sean112533 <zdte141@live.rhul.ac.uk>
2020-08-28 20:05:42 +0100

    make the test case for the BestTimeToSell.java

--- EasyQuestions/src/code/BestTimeToSell.java
+++ EasyQuestions/src/code/BestTimeToSell.java
@@ -2,4 +2,9 @@ package code;
 
 public class BestTimeToSell {
 
+  public int maxProfit(int[] prices) {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
 }
--- EasyQuestions/src/test/TestBestTimeToSell.java
+++ EasyQuestions/src/test/TestBestTimeToSell.java
@@ -1,11 +1,21 @@
 package test;
 
+import static org.junit.Assert.assertTrue;
 import org.junit.Before;
+import org.junit.Test;
+import code.BestTimeToSell;
 
 public class TestBestTimeToSell {
 
   @Before
   public void testClass() {
     BestTimeToSell test = new BestTimeToSell();
+  }
+
+  @Test
+  public void testMaxProfit() {
+    BestTimeToSell test = new BestTimeToSell();
+    int[] prices = {3, 3, 5, 0, 0, 3, 1, 4};
+    assertTrue("find the maximum profit of most two transaction", test.maxProfit(prices) == 6);
   }
 }
