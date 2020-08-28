8428219b593797bb2b20af5491128ffb170671b8
sean112533 <zdte141@live.rhul.ac.uk>
2020-08-28 19:38:32 +0100

    check and finish the sellStock

--- EasyQuestions/src/code/SellStock.java
+++ EasyQuestions/src/code/SellStock.java
@@ -1,7 +1,18 @@
 package code;
 
+/**
+ * Q121 Best time to buy and sell stock.
+ * 
+ * @author sanghunlee
+ *
+ */
 public class SellStock {
-
+  /**
+   * method maxProfit to compare the previous array of integer to find the most profit number.
+   * 
+   * @param prices array of integer with certain numbers.
+   * @return maximum profit from the array of integer values.
+   */
   public int maxProfit(int[] prices) {
     int maxProf = 0, j = 0;
     for (int i = 1; i < prices.length; i++) {
@@ -15,6 +26,11 @@ public class SellStock {
     return maxProf;
   }
 
+  /**
+   * main method to run the method.
+   * 
+   * @param args
+   */
   public static void main(String[] args) {
     SellStock test = new SellStock();
     int[] prices = {7, 1, 5, 3, 6, 4};
