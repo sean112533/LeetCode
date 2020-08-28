862ae64be22b97c5edcece3c506e71f11fc7bd81
sean112533 <zdte141@live.rhul.ac.uk>
2020-08-28 19:24:51 +0100

    make the test case for the sellStock

--- EasyQuestions/src/test/TestSellStock.java
+++ EasyQuestions/src/test/TestSellStock.java
@@ -1,5 +1,22 @@
 package test;
 
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.Test;
+import code.SellStock;
+
 public class TestSellStock {
 
+  @Before
+  public void testClass() {
+    SellStock test = new SellStock();
+  }
+
+  @Test
+  public void testMaxprofit() {
+    SellStock test = new SellStock();
+    int[] prices;
+    int i;
+    assertTrue("price of the day i in array is correct ", test.maxProfit(i) == 5);
+  }
 }
