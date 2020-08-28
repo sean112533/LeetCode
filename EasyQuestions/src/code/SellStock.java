7f720907fcebbc5ab5c625ba3ae1fdd5d2943c00
sean112533 <zdte141@live.rhul.ac.uk>
2020-08-28 19:36:37 +0100

    fnish making the sellStock and it's documentation

--- EasyQuestions/src/code/SellStock.java
+++ EasyQuestions/src/code/SellStock.java
@@ -2,4 +2,25 @@ package code;
 
 public class SellStock {
 
+  public int maxProfit(int[] prices) {
+    int maxProf = 0, j = 0;
+    for (int i = 1; i < prices.length; i++) {
+      int compare = prices[i] - prices[j];
+      if (compare > maxProf) {
+        maxProf = compare;
+      }
+      if (prices[i] < prices[j])
+        j = i;
+    }
+    return maxProf;
+  }
+
+  public static void main(String[] args) {
+    SellStock test = new SellStock();
+    int[] prices = {7, 1, 5, 3, 6, 4};
+    System.out.println(test.maxProfit(prices));
+  }
+
 }
+
+
--- EasyQuestions/src/test/TestSellStock.java
+++ EasyQuestions/src/test/TestSellStock.java
@@ -6,17 +6,22 @@ import org.junit.Test;
 import code.SellStock;
 
 public class TestSellStock {
-
+  /**
+   * use the @Before to test whether the class does exist to run the test the method maxProfit.
+   */
   @Before
   public void testClass() {
     SellStock test = new SellStock();
   }
 
+  /**
+   * Test case to set the integer of arrays prices with method maxProfit. return true if the result
+   * and estimated number matches.
+   */
   @Test
   public void testMaxprofit() {
     SellStock test = new SellStock();
-    int[] prices;
-    int i;
-    assertTrue("price of the day i in array is correct ", test.maxProfit(i) == 5);
+    int[] prices = {7, 1, 5, 3, 6, 4};
+    assertTrue("price of the day i in array is correct ", test.maxProfit(prices) == 5);
   }
 }
